f382c9c62d0f6468b3c64f18c3e98fa507c18afc
GilR <gilr@pixellot.tv>
2020-03-30 23:10:08 +0300

    prittier format + sdfdsfds

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,72 +1,36 @@
-import Chef from './models/chefs';
-import {Meal, Filters} from './models/meals';
+import Chef from "./models/chefs";
+import { Meal, Filters } from "./models/meals";
 
 export const Chef = [
-  new Chef('גיל', '#f5428d'),
-  new Chef( 'אפי', '#f54242'),
-  new Chef('אמא מיכל', '#f5a442'),
-  new Chef('אמא טלי', '#f5d142'),
-  new Chef('איתי', '#368dff'),
-  new Chef('ניצן', '#368dff'),
-
+  new Chef("גיל", "#f5428d"),
+  new Chef("אפי", "#f54242"),
+  new Chef("אמא מיכל", "#f5a442"),
+  new Chef("אמא טלי", "#f5d142"),
+  new Chef("איתי", "#368dff"),
+  new Chef("ניצן", "#368dff")
 ];
 
 export const MEALS = [
   new Meal(
-    'm1',
-    'גיל',
-    'מרק כרובית',
-    'פשוט',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+    "m1",
+    "גיל",
+    "מרק כרובית",
+    "פשוט",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg",
     60,
-    [
-      'כרובית שלמה',
-      '3 תפוחי אדמה',
-      '1 פחית קרם קוקוס',
-      'מלח, פלפל, חוויאג'
-    ],
-    [
-      'שלב 1',
-      'שלב 2',
-      'שלב 3',
-      'שלב 4',
-      'שלב 5',
-      'שלב 6'
-    ],
-    new Filters(true,
-        true,
-        true,
-        true,
-        false,
-        false,
-        false)
+    ["כרובית שלמה", "3 תפוחי אדמה", "1 פחית קרם קוקוס", "מלח, פלפל, חוויאג"],
+    ["שלב 1", "שלב 2", "שלב 3", "שלב 4", "שלב 5", "שלב 6"],
+    new Filters(true, true, true, true, false, false, false)
   ),
   new Meal(
-    'm2',
-    'אפי',
-    'קציצות ברוקולי',
-    'בינוני',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+    "m2",
+    "אפי",
+    "קציצות ברוקולי",
+    "בינוני",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg",
     90,
-    [
-      'ברוקולי',
-      'ברוקולי',
-      'ברוקולי',
-      'ברוקולי'
-    ],
-    [
-      'שלב 1',
-      'שלב 2',
-      'שלב 3',
-      'שלב 4',
-      'שלב 5',
-    ],
-    new Filters(false,
-        false,
-        true,
-        false,
-        true,
-        false,
-        false)
+    ["ברוקולי", "ברוקולי", "ברוקולי", "ברוקולי"],
+    ["שלב 1", "שלב 2", "שלב 3", "שלב 4", "שלב 5"],
+    new Filters(false, false, true, false, true, false, false)
   )
-]
+];
--- models/meals.js
+++ models/meals.js
@@ -1,49 +1,25 @@
 class Meal {
-    constructor(
-      id,
-      maker,
-      title,
-      complexity,
-      image,
-      duration,
-      ingredients,
-      steps,
-      Filters
-    ) {
-      this.id = id;
-      this.maker = maker;
-      this.title = title;
-      this.image = image;
-      this.ingredients = ingredients;
-      this.steps = steps;
-      this.duration = duration;
-      this.complexity = complexity;
-      this.Filters = Filters;
-
-    }
+  constructor(
+    id,
+    chef,
+    title,
+    complexity,
+    image,
+    duration,
+    ingredients,
+    steps,
+    filters
+  ) {
+    this.id = id;
+    this.chef = chef;
+    this.title = title;
+    this.image = image;
+    this.ingredients = ingredients;
+    this.steps = steps;
+    this.duration = duration;
+    this.complexity = complexity;
+    this.filters = filters;
   }
-  
-  class Filters{
-    constructor(
-        isGlutenFree,
-        isVegan,
-        isVegetarian,
-        isLactoseFree,
-        isMilky,
-        isMeaty,
-        isDessert
-    ){
-        this.isGlutenFree = isGlutenFree;
-        this.isVegan = isVegan;
-        this.isVegetarian = isVegetarian;
-        this.isLactoseFree = isLactoseFree;
-        this.isMilky = isMilky;
-        this.isMeaty = isMeaty;
-        this.isDessert = isDessert;
-    }
+}
 
-  }
-  export default Meal;
-  export default Filters;
-
-  
+export default Meal;
