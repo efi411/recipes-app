ffa85a84b9c7a8ac715a2e50897b17e88bb317de
i345010 <efrat.rotem@sap.com>
2020-04-06 14:56:39 +0300

    format fix

--- components/DefaultText.js
+++ components/DefaultText.js
@@ -2,14 +2,14 @@ import React from "react";
 import { StyleSheet, Text } from "react-native";
 import Fonts from "../constants/Fonts";
 
-const DefaultText = props => {
+const DefaultText = (props) => {
   return (
     <Text style={{ ...styles.text, ...props.style }}>{props.children}</Text>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {}
+  text: {},
 });
 
 export default DefaultText;
