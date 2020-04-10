d98b4be09833942b4248c2767893e7553ec16fba
Efi <efi411@gmail.com>
2020-04-10 12:20:17 +0300

    Update DefaultText component with fonts (#15)
    Co-authored-by: i345010 <efrat.rotem@sap.com>

--- components/DefaultText.js
+++ components/DefaultText.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { Text } from "react-native";
 
 const DefaultText = (props) => {
+  const fontFamily = (context) => {
+    let family;
+    switch (context) {
+      case "title":
+        family = "Assistant-ExtraBold";
+        break;
+      case "button":
+        family = "Assistant-Bold";
+        break;
+      default:
+        family = "Assistant-Regular";
+    }
+
+    return { fontFamily: family };
+  };
+
   return (
-    <Text style={{ ...styles.text, ...props.style }}>{props.children}</Text>
+    <Text style={{ ...props.style, ...fontFamily(props.type) }}>
+      {props.children}
+    </Text>
   );
 };
-
-const styles = StyleSheet.create({
-  text: {},
-});
 
 export default DefaultText;
