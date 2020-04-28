dfe66106e63c43e92ffe21f43479cebf44be4fb2
i345010 <efrat.rotem@sap.com>
2020-04-28 17:38:01 +0300

    Add icons for opening the drawers and saving filters

--- navigation/Navigators.js
+++ navigation/Navigators.js
@@ -2,11 +2,23 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import ChefMealsScreen from "../screens/ChefMealsScreen";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
 import FilterScreen from "../screens/FiltersScreen";
 import Color from "../constants/Colors";
+
+const defaultOptions = {
+  headerStyle: {
+    backgroundColor: Color.primaryColor,
+  },
+  headerTitleAlign: "center",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+  headerTintColor: "white",
+};
 
 const DrawerNavigator = () => {
   const Drawer = createDrawerNavigator();
@@ -28,49 +40,56 @@ const DrawerNavigator = () => {
   );
 };
 
-const StackNavigatorFilters = () => {
+const menuIcon = (onPressFunc) => (
+  <Icon.Button
+    name="menu"
+    size={25}
+    backgroundColor={Color.primaryColor}
+    onPress={() => onPressFunc()}
+  />
+);
+
+const SaveIcon = () => (
+  <Icon.Button
+    name="save"
+    size={25}
+    backgroundColor={Color.primaryColor}
+    onPress={() => console.log("Saved!!!!!")}
+  />
+);
+
+const StackNavigatorFilters = (props) => {
+  const { navigation } = props;
+
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Color.primaryColor,
-        },
-        headerTitleAlign: "center",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-        headerTintColor: "white",
-      }}
-    >
+    <Stack.Navigator screenOptions={defaultOptions}>
       <Stack.Screen
         name="Filters"
         component={FilterScreen}
-        options={{ title: "פילטרים" }}
+        options={{
+          title: "פילטרים",
+          headerLeft: menuIcon.bind(this, navigation.openDrawer),
+          headerRight: SaveIcon,
+        }}
       />
     </Stack.Navigator>
   );
 };
 
-const StackNavigatorMeals = () => {
+const StackNavigatorMeals = (props) => {
+  const { navigation } = props;
+
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Color.primaryColor,
-        },
-        headerTitleAlign: "center",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-        headerTintColor: "white",
-      }}
-    >
+    <Stack.Navigator screenOptions={defaultOptions}>
       <Stack.Screen
         name="Categories"
         component={CategoriesScreen}
-        options={{ title: "משתמשים" }}
+        options={{
+          title: "משתמשים",
+          headerLeft: menuIcon.bind(this, navigation.openDrawer),
+        }}
       />
       <Stack.Screen
         name="ChefMeals"
