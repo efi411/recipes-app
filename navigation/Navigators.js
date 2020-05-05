56b7b1cf607aad408ce8a19c7367bc571bc1c1ad
i345010 <efrat.rotem@sap.com>
2020-05-06 02:36:28 +0300

    Remove redundant Icon import

--- navigation/Navigators.js
+++ navigation/Navigators.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import Icon from "react-native-vector-icons/MaterialIcons";
 
 import CategoriesScreen, {
   categoriesScreenOptions,
