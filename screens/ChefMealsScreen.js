0a34a55913b368664a82ac4317c8d038b4615182
i345010 <efrat.rotem@sap.com>
2020-05-10 00:09:42 +0300

    Add redux functionality

--- screens/ChefMealsScreen.js
+++ screens/ChefMealsScreen.js
@@ -4,14 +4,15 @@
  *******************************************/
 
 import React from "react";
-import { MEALS } from "../data/dummy-data";
 import MealsList from "../components/MealsList";
+import { useSelector } from "react-redux";
 
 const ChefMealsScreen = (props) => {
   const { navigation } = props;
   let { chefId } = props.route.params;
 
-  const chefMeals = MEALS.filter((meal) => meal.chef == chefId);
+  const filteredMeals = useSelector((state) => state.meals.filteredMeals);
+  const chefMeals = filteredMeals.filter((meal) => meal.chef == chefId);
 
   return (
     <MealsList
